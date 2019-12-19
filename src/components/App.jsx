1dd42aeebb330940916eb611e6316012e629ac48
emil nikolov <emil.nikolov@appliedtechnology.se>
2019-12-19 20:30:53 +0100

    set up sockets

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Provider } from 'react-redux';
 import store from 'redux/store';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ReactNotifications from 'react-notifications-component';
-import axios from 'axios';
-import config from 'config.json';
+import io from 'socket.io-client';
+
+import Notice from 'components/reusables/Notice';
 
 import Login from 'components/Login';
 import Chat from 'components/Chat';
 import Footer from 'components/Footer';
 
-axios.defaults.baseURL = config.proxy;
+const socket = io(require('config.json').server);
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <div className='App'>
-        <Switch>
-          <Route exact path='/' component={Login} />
-          <Route exact path='/chat' component={Chat} />
-        </Switch>
-        <Footer />
-        <ReactNotifications />
-      </div>
-    </Router>
-  </Provider>
-);
+socket.on('chat-message', message => console.log(message));
+
+socket.on('notification', obj => Notice(obj));
+
+const App = () => {
+  const [nickname, setNickname] = useState('');
+  const [message, setMessage] = useState('');
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className='App'>
+          <input
+            type='text'
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+          />
+          <button onClick={() => socket.emit('chat-message', message)}>
+            send message
+          </button>
+
+          <input
+            type='text'
+            value={nickname}
+            onChange={e => setNickname(e.target.value)}
+          />
+          <button onClick={() => socket.emit('setNickname', nickname)}>
+            change nick
+          </button>
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route exact path='/chat' component={Chat} />
+          </Switch>
+          <Footer />
+          <ReactNotifications />
+        </div>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
--- src/redux/actions/User.js
+++ src/redux/actions/User.js
@@ -1,52 +1,45 @@
 import { SET_NICKNAME, UNSET_NICKNAME } from 'redux/types';
-import axios from 'axios';
 import Notice from 'components/reusables/Notice';
 
 export const setNickname = nickname => async dispatch => {
-  try {
-    const { data } = await axios.get(`users/${nickname}`);
-
-    if (data.error) {
-      return Notice({
-        message: data.error,
-        type: 'danger',
-      });
-    }
-
-    Notice({ message: `Welcome to the chat, ${nickname}!`, type: 'success' });
-    dispatch({
-      type: SET_NICKNAME,
-      payload: nickname,
-    });
-    localStorage.ubiNickname = nickname;
-  } catch (_) {
-    Notice({
-      message: 'Looks like our servers are down. Please try again in a second!',
-      type: 'danger',
-    });
-  }
+  // try {
+  //   if (data.error) {
+  //     return Notice({
+  //       message: data.error,
+  //       type: 'danger',
+  //     });
+  //   }
+  //   Notice({ message: `Welcome to the chat, ${nickname}!`, type: 'success' });
+  //   dispatch({
+  //     type: SET_NICKNAME,
+  //     payload: nickname,
+  //   });
+  //   localStorage.ubiNickname = nickname;
+  // } catch (_) {
+  //   Notice({
+  //     message: 'Looks like our servers are down. Please try again in a second!',
+  //     type: 'danger',
+  //   });
+  // }
 };
 
 export const disconnect = nickname => async dispatch => {
-  try {
-    const { data } = await axios.delete(`users/${nickname}`);
-
-    if (data.error) {
-      return Notice({
-        message: data.error,
-        type: 'danger',
-      });
-    }
-
-    Notice({ message: `We will miss you, ${nickname}!`, type: 'info' });
-    dispatch({
-      type: UNSET_NICKNAME,
-    });
-    delete localStorage.ubiNickname;
-  } catch (_) {
-    Notice({
-      message: 'Looks like our servers are down. Please try again in a second!',
-      type: 'danger',
-    });
-  }
+  // try {
+  //   if (data.error) {
+  //     return Notice({
+  //       message: data.error,
+  //       type: 'danger',
+  //     });
+  //   }
+  //   Notice({ message: `We will miss you, ${nickname}!`, type: 'info' });
+  //   dispatch({
+  //     type: UNSET_NICKNAME,
+  //   });
+  //   delete localStorage.ubiNickname;
+  // } catch (_) {
+  //   Notice({
+  //     message: 'Looks like our servers are down. Please try again in a second!',
+  //     type: 'danger',
+  //   });
+  // }
 };
